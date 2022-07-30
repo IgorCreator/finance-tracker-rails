6ab9983ef1b990ec1171b65451246b2521b2cd97
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-30 17:02:14 -0400

    Allow first and last names during signup and edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "admin@gmail.com",
+  password: '123456789',
+  first_name: "Admin",
+  last_name: ""
+)
 
 User.create!(
   email: 'user@domain.com',
   password: '123456789',
+  first_name: "user",
+  last_name: "Second"
 )
 
 User.create!(
   :email => 'john@gmail.com',
-  :password => 'topsecret',
+  :password => '123456789',
+  first_name: "John",
+  last_name: "Third"
+)
+
+User.create!(
+  :email => 'tom_user@gmail.com',
+  :password => '123456789',
+  first_name: "Tom",
+  last_name: "User"
 )
